04dacd83395205313842034dc417ff4334d04568
MyronMaharaj <31753408+MyronMaharaj@users.noreply.github.com>
2017-10-09 11:10:08 -0400

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,20 @@
+/* Lab1 first file - ID number 96013952*/
+#include <p18f452.h>
+
+unsigned char counter;
+
+/* Set configuration bits for use with ICD2*/
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+void main(void)
+{
+    TRISB=0;
+    counter=1;
+    while(counter<16)
+    {
+        PORTB=counter;
+        counter=counter+1;
+    }
+}
